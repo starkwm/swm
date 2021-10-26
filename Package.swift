eedd2f656247a1058ed638e2b4fc851e535dddb2
Tom Bell <tomb@tomb.io>
2021-10-26 01:17:09 +0100

    Update swift-argument-parser to 1.0.1

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v10_15),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.2"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.1"),
     ],
     targets: [
         .target(
