7bef1101cf216705b932b2df19b5ee42ef07395c
Tom Bell <tomb@tomb.io>
2021-10-29 00:11:50 +0100

    Update to inline defaultConfigPath

--- Sources/swm/Arguments.swift
+++ Sources/swm/Arguments.swift
@@ -1,14 +1,12 @@
 import ArgumentParser
 import Foundation
 
-let defaultConfigPath = ("~/.config/swm/swmrc" as NSString).resolvingSymlinksInPath
-
 struct Arguments: ParsableArguments {
     @Flag(name: .shortAndLong, help: "Show version information.")
     var version = false
 
     @Option(name: .shortAndLong, help: ArgumentHelp("Path to the configuration file.", valueName: "path"))
-    var config: String = defaultConfigPath
+    var config: String = ("~/.config/swm/swmrc" as NSString).resolvingSymlinksInPath
 
     @Flag(name: .shortAndLong, help: "")
     var message = false
