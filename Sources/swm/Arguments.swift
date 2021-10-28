82e47cb23dd103727026dd81e904a4221e27c1da
Tom Bell <tomb@tomb.io>
2021-10-28 22:50:29 +0100

    Remove comments from argument flags

--- Sources/swm/Arguments.swift
+++ Sources/swm/Arguments.swift
@@ -4,15 +4,12 @@ import Foundation
 let defaultConfigPath = ("~/.config/swm/swmrc" as NSString).resolvingSymlinksInPath
 
 struct Arguments: ParsableArguments {
-    // --version/-v flag
     @Flag(name: .shortAndLong, help: "Show version information.")
     var version = false
 
-    // --config/-c
     @Option(name: .shortAndLong, help: ArgumentHelp("Path to the configuration file.", valueName: "path"))
     var config: String = defaultConfigPath
 
-    // --message/-m
     @Flag(name: .shortAndLong, help: "")
     var message = false
 }
