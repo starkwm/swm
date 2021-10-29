86606641f4b8cf6b77c964c9d0ea0c70d8de9dc2
Tom Bell <tomb@tomb.io>
2021-10-29 18:25:03 +0100

    Add skylight dependency for testing private frameworks

--- Package.swift
+++ Package.swift
@@ -5,18 +5,28 @@ import PackageDescription
 let package = Package(
     name: "swm",
     platforms: [
-        .macOS(.v11),
+        .macOS(.v12),
     ],
     dependencies: [
         .package(url: "https://github.com/Kitura/BlueSocket.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.5.0"),
+        .package(url: "https://github.com/tombell/skylight", from: "0.0.1"),
     ],
     targets: [
         .executableTarget(
             name: "swm",
             dependencies: [
-              "swmlib",
-              .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "swmlib",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "SkyLight", package: "skylight"),
+            ],
+            linkerSettings: [
+                .unsafeFlags([
+                    "-Xlinker", "-F",
+                    "-Xlinker", "/System/Library/PrivateFrameworks",
+                    "-Xlinker", "-framework",
+                    "-Xlinker", "SkyLight",
+                ]),
             ]),
         .target(
             name: "swmlib",
