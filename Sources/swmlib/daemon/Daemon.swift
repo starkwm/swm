08c89b20d01ca56edd7e4ba469c7b1e5cc0a6143
Tom Bell <tomb@tomb.io>
2021-10-31 19:41:38 +0000

    Remove passing unused args to main()

--- Sources/swmlib/daemon/Daemon.swift
+++ Sources/swmlib/daemon/Daemon.swift
@@ -1,3 +1,20 @@
 import Foundation
+import Socket
 
-struct Daemon {}
+public struct Daemon {
+    private static let maxReadBufferSize = 1024
+
+    var listen: Socket?
+
+    private static func socketFilePath() throws -> String {
+        guard let user = ProcessInfo.processInfo.environment["USER"] else {
+            throw DaemonError.userEnvVarMissing
+        }
+
+        return FileManager
+            .default
+            .temporaryDirectory
+            .appendingPathComponent("swm_\(user).sock", isDirectory: false)
+            .path
+    }
+}
