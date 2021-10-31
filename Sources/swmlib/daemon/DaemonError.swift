9f639a41954f6ac1d98a930670f39601a28d0a7f
Tom Bell <tomb@tomb.io>
2021-10-31 22:00:28 +0000

    Add additional daemon errors

--- Sources/swmlib/daemon/DaemonError.swift
+++ Sources/swmlib/daemon/DaemonError.swift
@@ -1,5 +1,8 @@
 public enum DaemonError: Error {
     case userEnvVarMissing
+    case unableToCreateSocket
+    case unableToUnwrapSocket
+    case unableToListenOnSocket
 }
 
 extension DaemonError: CustomStringConvertible {
@@ -7,6 +10,12 @@ extension DaemonError: CustomStringConvertible {
         switch self {
         case .userEnvVarMissing:
             return "USER environment variable is not set"
+        case .unableToCreateSocket:
+            return "unable to create listening socket"
+        case .unableToUnwrapSocket:
+            return "unable to unwrap listening socket"
+        case .unableToListenOnSocket:
+            return "unable to listen on listening socket"
         }
     }
 }
