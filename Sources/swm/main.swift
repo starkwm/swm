cef6500d8c42b310f301377f188f052c99404d61
Tom Bell <tomb@tomb.io>
2021-11-01 21:16:55 +0000

    Move arguments back into main.swift

--- Sources/swm/main.swift
+++ Sources/swm/main.swift
@@ -1,7 +1,21 @@
 import AppKit
+import ArgumentParser
 import swmlib
 
 let version = "0.0.1"
+
+struct Arguments: ParsableArguments {
+    @Flag(name: .shortAndLong, help: "Show version information.")
+    var version = false
+
+    @Option(name: .shortAndLong, help: ArgumentHelp("Path to the configuration file.", valueName: "path"))
+    var config: String = ("~/.config/swm/swmrc" as NSString).resolvingSymlinksInPath
+
+    @Flag(name: .shortAndLong, help: "")
+    var message = false
+}
+
+let arguments = Arguments.parseOrExit()
 
 func main(args _: [String]) -> Int32 {
     if arguments.version {
