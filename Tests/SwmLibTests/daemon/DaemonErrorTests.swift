18262fce58fcd2d920270c4dfc881407094f6342
Tom Bell <tomb@tomb.io>
2021-11-06 11:38:16 +0000

    Add remaining tests

--- Tests/SwmLibTests/daemon/DaemonErrorTests.swift
+++ Tests/SwmLibTests/daemon/DaemonErrorTests.swift
@@ -9,4 +9,28 @@ final class DaemonErrorTests: XCTestCase {
             XCTAssertEqual("\(error)", "USER environment variable is not set")
         }
     }
+
+    func testUnableToCreateSocketError() {
+        do {
+            throw DaemonError.unableToCreateSocket
+        } catch {
+            XCTAssertEqual("\(error)", "unable to create listening socket")
+        }
+    }
+
+    func testUnableToUnwrapSocketError() {
+        do {
+            throw DaemonError.unableToUnwrapSocket
+        } catch {
+            XCTAssertEqual("\(error)", "unable to unwrap listening socket")
+        }
+    }
+
+    func testUnableToListenOnSocketError() {
+        do {
+            throw DaemonError.unableToListenOnSocket
+        } catch {
+            XCTAssertEqual("\(error)", "unable to listen on listening socket")
+        }
+    }
 }
