f9ab10ca6fff6f9a96c9d98d85b84e21ccff91b3
Tom Bell <tomb@tomb.io>
2021-11-06 22:42:42 +0000

    Remove message flag

--- Sources/swm/main.swift
+++ Sources/swm/main.swift
@@ -10,9 +10,6 @@ struct Arguments: ParsableArguments {
 
     @Option(name: .shortAndLong, help: ArgumentHelp("Path to the configuration file.", valueName: "path"))
     var config: String = ("~/.config/swm/swmrc" as NSString).resolvingSymlinksInPath
-
-    @Flag(name: .shortAndLong, help: "")
-    var message = false
 }
 
 let arguments = Arguments.parseOrExit()
