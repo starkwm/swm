267873c351846dec34d9725fbfce32ce6b7d2c35
Tom Bell <tomb@tomb.io>
2021-11-10 19:47:33 +0000

    Update MessageClient

--- Sources/swmlib/messaging/MessageClient.swift
+++ Sources/swmlib/messaging/MessageClient.swift
@@ -2,8 +2,15 @@ import Foundation
 import Socket
 
 public enum MessageClient {
-    public static func send(message _: MessageDomain, args _: [String]) throws {
-        let socket = try Socket.create(family: .unix)
-        try socket.connect(to: try Daemon.socketFilePath())
+    public static func send(message: MessageDomain, args: [String]) {
+        do {
+            let socket = try Socket.create(family: .unix)
+            try socket.connect(to: try Daemon.socketFilePath())
+            try socket.write(from: "\(message) \(args.joined(separator: " "))")
+            exit(EXIT_SUCCESS)
+        } catch {
+            print("error: \(error)")
+            exit(EXIT_FAILURE)
+        }
     }
 }
