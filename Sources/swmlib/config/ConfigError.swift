bcb9e8a586cd14f19e032b6d23e3c9594784d6d3
Tom Bell <tomb@tomb.io>
2021-11-10 23:16:43 +0000

    Add Config num for executing the configuration file

---
+++ Sources/swmlib/config/ConfigError.swift
@@ -0,0 +1,18 @@
+enum ConfigError: Error {
+    case fileDoesNotExist
+    case unableToMakeExecutable
+    case unableToExecute
+}
+
+extension ConfigError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .fileDoesNotExist:
+            return "configuration file does not exist"
+        case .unableToMakeExecutable:
+            return "unable to mark the configuration file as executable"
+        case .unableToExecute:
+            return "unable to execute the configuration file"
+        }
+    }
+}
