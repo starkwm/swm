08089a5f4dc93d474fb05045b4c673bcadb57bbd
Tom Bell <tomb@tomb.io>
2023-08-13 22:45:18 +0100

    Upate Application initializer and properties

--- Sources/swmlib/accessibility/Application.swift
+++ Sources/swmlib/accessibility/Application.swift
@@ -1,9 +1,17 @@
 import AppKit
 
 public struct Application {
-    public var element: AXUIElement
+    private var element: AXUIElement
+
+    private var app: NSRunningApplication
 
     public init(pid: pid_t) {
         element = AXUIElementCreateApplication(pid)
+        app = NSRunningApplication(processIdentifier: pid)!
+    }
+
+    public init(app: NSRunningApplication) {
+        element = AXUIElementCreateApplication(app.processIdentifier)
+        self.app = app
     }
 }
