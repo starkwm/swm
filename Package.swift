1479137859b08756a34009485d5452fab3e9321e
Tom Bell <tomb@tomb.io>
2024-04-24 00:38:35 +0100

    Update Package.swift to remove skylight package

--- Package.swift
+++ Package.swift
@@ -3,38 +3,36 @@
 import PackageDescription
 
 let package = Package(
-    name: "swm",
-    platforms: [
-        .macOS(.v14),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Kitura/BlueSocket.git", from: "2.0.2"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
-        .package(url: "https://github.com/starkwm/skylight", from: "0.0.1"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "swm",
-            dependencies: [
-                "swmlib",
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "SkyLight", package: "skylight"),
-            ],
-            linkerSettings: [
-                .unsafeFlags([
-                    "-Xlinker", "-F",
-                    "-Xlinker", "/System/Library/PrivateFrameworks",
-                    "-Xlinker", "-framework",
-                    "-Xlinker", "SkyLight",
-                ]),
-            ]
-        ),
-        .target(
-            name: "swmlib",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Socket", package: "BlueSocket"),
-            ]
-        ),
-    ]
+  name: "swm",
+  platforms: [
+    .macOS(.v14)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Kitura/BlueSocket.git", from: "2.0.2"),
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "swm",
+      dependencies: [
+        "swmlib",
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ],
+      linkerSettings: [
+        .unsafeFlags([
+          "-Xlinker", "-F",
+          "-Xlinker", "/System/Library/PrivateFrameworks",
+          "-Xlinker", "-framework",
+          "-Xlinker", "SkyLight",
+        ])
+      ]
+    ),
+    .target(
+      name: "swmlib",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "Socket", package: "BlueSocket"),
+      ]
+    ),
+  ]
 )
