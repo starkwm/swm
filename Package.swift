d987c97b4065508a17082bf60cb5c31ed4212633
Tom Bell <tomb@tombell.dev>
2026-05-03 22:46:56 +0100

    Update swift version to 6.2

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 
 import PackageDescription
 
 let package = Package(
   name: "swm",
   platforms: [
-    .macOS(.v14)
+    .macOS(.v26)
   ],
   products: [
     .executable(name: "swm", targets: ["Swm"])
