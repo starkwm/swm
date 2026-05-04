5f87a25b1acc36682bb37816f3a86344a2043b2d
Tom Bell <tomb@tombell.dev>
2026-05-04 19:59:30 +0100

    Order runtime declarations

--- Sources/SwmLib/Runtime/Logging/Log.swift
+++ Sources/SwmLib/Runtime/Logging/Log.swift
@@ -1,14 +1,14 @@
 import Foundation
 
+func log(_ message: @autoclosure () -> String, level: LogLevel = .debug) {
+  let text = "\(Date().ISO8601Format()) \(level.rawValue): \(message())\n"
+  fputs(text, stderr)
+  fflush(stderr)
+}
+
 enum LogLevel: String {
   case debug = "DEBUG"
   case info = "INFO"
   case warn = "WARN"
   case error = "ERROR"
-}
-
-func log(_ message: @autoclosure () -> String, level: LogLevel = .debug) {
-  let text = "\(Date().ISO8601Format()) \(level.rawValue): \(message())\n"
-  fputs(text, stderr)
-  fflush(stderr)
 }
