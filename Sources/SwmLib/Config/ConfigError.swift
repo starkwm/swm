144372c4bd5a488cc118dc76482a6150869d826f
Tom Bell <tomb@tombell.dev>
2026-05-09 13:48:40 +0100

    Document config errors

--- Sources/SwmLib/Config/ConfigError.swift
+++ Sources/SwmLib/Config/ConfigError.swift
@@ -1,11 +1,20 @@
+/// Errors that can occur while preparing or running the user's configuration file.
 enum ConfigError: Error {
+  /// The configuration file path does not exist.
   case fileDoesNotExist
+
+  /// The configuration file could not be marked executable by its owner.
   case unableToMakeExecutable
+
+  /// The configuration file could not be launched.
   case unableToExecute
+
+  /// The configuration file launched but exited unsuccessfully.
   case configurationFailed(status: Int32)
 }
 
 extension ConfigError: CustomStringConvertible {
+  /// A user-facing explanation of the configuration failure.
   var description: String {
     switch self {
     case .fileDoesNotExist:
