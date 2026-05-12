81b18452f9e12c2c056e648c5977d57aa360740a
Tom Bell <tomb@tombell.dev>
2026-05-12 19:17:33 +0100

    Update to version v0.0.7

--- Sources/Swm/Version.swift
+++ Sources/Swm/Version.swift
@@ -1,7 +1,7 @@
 /// Build version embedded in the swm executable.
 struct Version {
   /// Current generated version.
-  static let current = Self(value: "v0.0.6")
+  static let current = Self(value: "v0.0.7")
 
   /// Version string printed by `swm --version`.
   let value: String
