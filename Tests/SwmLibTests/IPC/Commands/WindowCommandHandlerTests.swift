625306e0d85d5d03ffd68a4e4faa0cd9470f4d2b
Tom Bell <tomb@tombell.dev>
2026-08-31 11:07:06 +0100

    Add directional window focus

--- Tests/SwmLibTests/IPC/Commands/WindowCommandHandlerTests.swift
+++ Tests/SwmLibTests/IPC/Commands/WindowCommandHandlerTests.swift
@@ -187,6 +187,25 @@ struct WindowCommandHandlerTests {
   }
 }
 
+@Suite("WindowFocusTarget")
+struct WindowFocusTargetTests {
+  @Test("init: accepts selectors and cardinal directions")
+  func initAcceptsSelectorsAndCardinalDirections() {
+    #expect(WindowFocusTarget(arguments: []) == .selected(nil))
+    #expect(WindowFocusTarget(arguments: ["recent"]) == .selected("recent"))
+    #expect(WindowFocusTarget(arguments: ["42"]) == .selected("42"))
+    #expect(WindowFocusTarget(arguments: ["left"]) == .direction(.left))
+    #expect(WindowFocusTarget(arguments: ["right"]) == .direction(.right))
+    #expect(WindowFocusTarget(arguments: ["up"]) == .direction(.up))
+    #expect(WindowFocusTarget(arguments: ["down"]) == .direction(.down))
+  }
+
+  @Test("init: rejects multiple arguments")
+  func initRejectsMultipleArguments() {
+    #expect(WindowFocusTarget(arguments: ["42", "left"]) == nil)
+  }
+}
+
 @Suite("WindowDisplayTarget")
 struct WindowDisplayTargetTests {
   @Test("init: accepts relative and indexed targets")
